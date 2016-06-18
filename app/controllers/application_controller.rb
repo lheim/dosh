e656227c2061ae30c4765274e18bbab1c74686bf
Lennart Heim <lennart.heim@icloud.com>
2016-06-18 16:44:01 +0200

    Fixed flash messages, added the missing buttons for the container control, added more information-panels for the container

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  add_flash_types :success, :warning, :danger, :info
+  add_flash_types :success, :warning, :danger, :info, :error
 
 
 
--- app/controllers/containers_controller.rb
+++ app/controllers/containers_controller.rb
@@ -6,42 +6,66 @@ class ContainersController < ApplicationController
 
   def show
     @container = Docker::Container.get(params[:id])
+    @logs = @container.logs(stdout: true, stderr: true)
   end
 
 
   def start
     @container = Docker::Container.get(params[:id])
     @container.start
-
-    redirect_to container_path, success: "container sucessfully started."
-
+    #redefine @container
+    @container = Docker::Container.get(params[:id])
+    state = @container.info['State']['Status']
+    if state == 'running'
+      redirect_to container_path, success: "container successfully started."
+    else
+      redirect_to container_path, error: "container failed to start."
+    end
   end
 
   def stop
     @container = Docker::Container.get(params[:id])
     @container.stop
-
-
+    #redefine @container
+    @container = Docker::Container.get(params[:id])
     state = @container.info['State']['Status']
     if state == 'exited'
-      flash[:success] = 'container sucessfully stopped.'
-      redirect_to container_path
+      redirect_to container_path, success: "container successfully stopped."
     else
-      flash[:error] = 'container failed to stop.'
-      redirect_to container_path
+      redirect_to container_path, error: "container failed to stop."
     end
   end
 
   def restart
-
+    @container = Docker::Container.get(params[:id])
+    @container.restart
+    #redefine @container
+    @container = Docker::Container.get(params[:id])
+    state = @container.info['State']['Status']
+    if state == 'running'
+      redirect_to container_path, success: "container successfully restarted."
+    else
+      redirect_to container_path, error: "container failed to restart."
+    end
   end
 
   def remove
-
+    @container = Docker::Container.get(params[:id])
+    state = @container.info['State']['Status']
+    if state == 'exited'
+      @container.remove
+      redirect_to containers_path, success: "container was successfully removed."
+    elsif state == 'running'
+      redirect_to container_path, error: "stop the container first before removing."
+    else
+      redirect_to container_path, error: "error: couldn't be removed."
+    end
   end
 
   def commit
-
+    @container = Docker::Container.get(params[:id])
+    @container.commit
+    redirect_to container_path, success: "container successfully committed."
   end
 
   def new
