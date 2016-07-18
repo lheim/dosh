d01fe098ba0c3c9cd289e3199b16df96d168859a
Lennart Heim <lennart.heim@rwth-aachen.de>
2016-07-18 17:50:42 +0200

    commit before creating a database

--- app/controllers/usrp_controller.rb
+++ app/controllers/usrp_controller.rb
@@ -3,9 +3,28 @@ class UsrpController < ApplicationController
 
     @stdout, @stdeerr, @status = Open3.capture3('uhd_find_devices')
 
+  end
 
 
-
+  #check for new usrp
+  def refresh
 
   end
+
+
+  #add discovered usrp to database
+  def add
+
+  end
+
+  #delete usrp entry from database
+  def delete
+
+  end
+
+  #create a new container with given usrp
+  def create
+
+  end
+
 end
